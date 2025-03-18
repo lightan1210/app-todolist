5f2b4e29fe9bdf318e76a3c9e3163fa3bb4981a3
Norberto <norbertoj54@hotmail.com>
2025-03-18 12:19:58 -0300

    Retouching ToDoElement and add modal option

--- src/Components/ToDoElement.js
+++ src/Components/ToDoElement.js
@@ -2,8 +2,10 @@ import { useEffect, useRef, useState } from "react";
 
 const ToDoElement = ({ deleteToDo, id, description }) => {
     const [stateToDo, setStateToDo] = useState(false);
+    let descriptionToDo = description;
 
     const toDoElement = useRef();
+    const modalElement = useRef();
 
     useEffect(() => {
         if(stateToDo)
@@ -14,12 +16,24 @@ const ToDoElement = ({ deleteToDo, id, description }) => {
     },[stateToDo])
 
     return (
-        <div ref={toDoElement} className="toDoElement">
-            <h2>TAREA</h2>
-            <p>{description}</p>
-            <input type="checkbox" onChange={(e) => setStateToDo(e.target.checked)}></input>
-            <div className="removeToDo" onClick={() => deleteToDo(id)}> X </div>
-        </div>
+        <>
+            <div ref={toDoElement} className="toDoElement">
+                <h2>TAREA</h2>
+                <p>{description}</p>
+                <input type="checkbox" onChange={(e) => setStateToDo(e.target.checked)}></input>
+                <div className="removeToDo" onClick={() => deleteToDo(id)}> X </div>
+                <div className="seeMore" onClick={() => modalElement.current.showModal()}>Ver mas</div>
+            </div>
+            
+            <dialog className="modalDialog" closedby='any' ref={modalElement}>
+                <form>
+                    <button formMethod="dialog">X</button>
+                </form>
+                <p>
+                    {descriptionToDo}
+                </p>
+            </dialog>
+        </>
     )
 }
 
--- src/Components/ToDoList.js
+++ src/Components/ToDoList.js
@@ -5,8 +5,9 @@ let nextId = 0;
 
 const ToDoList = () => {
 
+    const initialToDos = [{id:nextId, description:"Cillum labore laborum pariatur anim dolore fugiat nostrud culpa cillum Lorem commodo ad excepteur nulla. Velit excepteur ut excepteur nulla ut deserunt quis. Qui tempor consequat non incididunt irure minim minim. Exercitation esse fugiat Lorem dolore irure est aliqua anim Lorem veniam elit. Deserunt sint voluptate dolore consequat dolore qui nostrud ut."}];
     
-    const [todos, setTodos] = useState([]);
+    const [todos, setTodos] = useState(initialToDos);
     const [descriptionToDo, setDescriptionToDo] = useState('');
     
     const addToDo = () => {
@@ -21,11 +22,11 @@ const ToDoList = () => {
 
     return (
         <div className="toDoList">
-            <h1>LISTA DE TAREAS</h1>
             <div className="newToDoField">
                 <input type='text' onChange={(e) => setDescriptionToDo(e.target.value)}></input>
                 <button onClick={() => addToDo()}>Agregar tarea</button>
             </div>
+            <h1>LISTA DE TAREAS</h1>
             <div className="todos">
                 {todos.map((a, key) => {
                     return( <ToDoElement deleteToDo={deleteToDo} key={key} id={a.id} description={a.description + a.id} /> )
